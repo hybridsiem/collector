e9656277eec8de995d9fc5bf868ac2584dc76a75
hybridsiem <45217073+hybridsiem@users.noreply.github.com>
2018-11-21 02:45:54 +0200

    Add files via upload

---
+++ support-dev.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+tmp=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.1.1 | awk '{print $3}'`
+#echo "$tmp"
+if [ -z $tmp ]
+then
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.1.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+fi
+
+tmp1=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.2.1 | awk '{print $3}'`
+#echo "$tmp1"
+if [ -z $tmp1 ]
+then
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.2.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+fi
+
+tmp2=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.3.1 | awk '{print $3}'`
+#echo "$tmp2"
+if [ -z $tmp2 ]
+then
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.3.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+fi
+
+
+
+
+
+echo "Please provide to BESECURE the following address for remote support"
+ifconfig | grep tun10001 -C1 | awk '{print $2}'
+
