8270fc590e5cd6266ba56daf60673ed567636856
hybridsiem <45217073+hybridsiem@users.noreply.github.com>
2018-11-21 02:40:04 +0200

    Add files via upload

--- support.sh
+++ support.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
-tmp1=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.11.1 | awk '{print $3}'`
-#echo "$tmp1"
-if [ -z $tmp1 ]
+tmp=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.11.1 | awk '{print $3}'`
+#echo "$tmp"
+if [ -z $tmp ]
 then
-/opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.11.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.11.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 fi
 
 tmp1=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.12.1 | awk '{print $3}'`
+#echo "$tmp1"
+if [ -z $tmp1 ]
+then
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.12.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+fi
+
+tmp2=`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.13.1 | awk '{print $3}'`
 #echo "$tmp2"
 if [ -z $tmp2 ]
 then
-/opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.12.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        /opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.13.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 fi
 
-tmp=3`/opt/immune/bin/li-admin/iptables.sh -nvL INPUT | grep 10.98.13.1 | awk '{print $3}'`
-#echo "$tmp3"
-if [ -z $tmp3 ]
-then
-/opt/immune/bin/li-admin/iptables.sh -I INPUT 2 -p tcp -s 10.98.13.1 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-fi
+
+
 
 
 echo "Please provide to BESECURE the following address for remote support"
-
 ifconfig | grep tun10001 -C1 | awk '{print $2}'
+
